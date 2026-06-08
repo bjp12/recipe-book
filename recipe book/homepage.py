30ebc1467766ae8a32cf5019ee8afc4fceb948c6
Bijal Patel <bijaljpatel12@gmail.com>
2026-06-08 11:36:39 -0500

    Disable debug mode in homepage.py

--- recipe book/homepage.py
+++ recipe book/homepage.py
@@ -175,5 +175,5 @@ def about():
     return render_template('aboutus.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5004, debug=True)
+    app.run(host='0.0.0.0', port=5004, debug=False)
 
